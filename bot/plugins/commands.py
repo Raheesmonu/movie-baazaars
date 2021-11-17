afe92f449894815dc3f4fbeaae3f865700b24e97
Raheesmonu <85986824+Raheesmonu@users.noreply.github.com>
2021-11-17 14:48:54 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⚜️OUR GROUP⚜️', url="https://t.me/joinchat/2kBSBV7gWbQwYzI1"
                                 )
                         ]
                     ]
@@ -47,12 +47,7 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
-    ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
-    ],[
-        InlineKeyboardButton('Help ⚙', callback_data="help")
+        InlineKeyboardButton('⚜️OUR CHANNEL ⚜️', url='https://t.me/joinchat/2kBSBV7gWbQwYzI1'),
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
