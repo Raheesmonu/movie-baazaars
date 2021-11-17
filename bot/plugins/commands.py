c7882fc45b33ea6e2bbb99e1889cd630293071c5
Raheesmonu <85986824+Raheesmonu@users.noreply.github.com>
2021-11-17 19:50:07 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f"{file_name} /n movie_baazaars",
+                caption = f"📂Movie_Baazaars </n>{file_name}  ",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
